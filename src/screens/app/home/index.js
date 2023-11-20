a1a75d60cb018a8952416bc2ff136c79a1557549
ShoaibVirk008 <hf@HFs-MacBook-Pro.local>
2023-11-20 16:59:41 +0500

    post detail added

--- src/screens/app/home/index.js
+++ src/screens/app/home/index.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Wrapper, Text, Images, Spacer, Icons, Buttons, ScrollViews, HeaderHome } from '../../../components';
-import {useHooks} from './hooks'
+import { useHooks } from './hooks'
+import { navigate } from '../../../navigation/rootNavigation';
+import { routes } from '../../../services';
 
 
 export default function Home() {
   const { } = useHooks()
   return (
     <Wrapper isMain isCenter>
-           <Text isLargeTitle>Home</Text>
-        </Wrapper>
+      <Wrapper alignItemsCenter>
+        <Text isLargeTitle>Home</Text>
+        <Spacer isBasic />
+        <Text isMedium isGray
+          onPress={() => navigate(routes.postDetail)}
+        >View Post Details</Text>
+      </Wrapper>
+    </Wrapper>
   );
 }
 
